73415e480f538c130b80a98b11f5228ef64927fc
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2024-09-23 15:55:23 +0700

    Extracted all the common code into a seprate nestjs-defaults package

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
+import { getCurrentInvoke } from '@codegenie/serverless-express';
 
-@Controller('health')
+@Controller()
 export class AppController {
-  @Get()
-  check() {
-    return 'Ok';
+  private readonly logger = new Logger(AppController.name);
+
+  @Get('logme')
+  logMe() {
+    this.logger.log('this is a test message');
+    this.logger.log('this is a test message', 'foo');
+    this.logger.log('this is a test message', 'foo', { baz: 'bar' });
+    return 'ok';
+  }
+
+  @Get('goboom')
+  goBoom() {
+    try {
+      throw new Error('boom');
+    } catch (err) {
+      this.logger.error('this is a test message', err);
+      throw err;
+    }
+  }
+
+  @Get('debug')
+  debug() {
+    return {
+      ...getCurrentInvoke(),
+    };
+  }
+
+  @Get('goboom')
+  goboom() {
+    throw new Error('Oh no!');
   }
 }
